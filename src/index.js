28838d37efaff63b164689b9a65aa1397e8f1fa7
Thai Pangsakulyanont <dtinth@spacet.me>
2016-05-21 18:28:10 +0700

    Add the required fonts

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import App from './App'
 import h from 'react-hyperscript'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
+import { loadCSS } from 'fg-loadcss'
 import './index.styl'
 
 const routes = { path: '/*', component: App }
 const router = h(Router, { history: browserHistory, routes: routes })
 render(router, document.getElementById('react'))
+
+loadCSS('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic|Roboto+Mono:400,700')
