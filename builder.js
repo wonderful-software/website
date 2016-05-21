71c5b34dfcef7816fe0d2a19cf234eac7565b72f
Thai Pangsakulyanont <dtinth@spacet.me>
2016-05-22 00:11:50 +0700

    Inline all the CSS

--- builder.js
+++ builder.js
@@ -6,6 +6,13 @@ const template = require('fs').readFileSync('./build/assets/index.html', 'utf8')
 const mkdirp = require('mkdirp')
 const path = require('path')
 
+function inlineStyle (html) {
+  if (!html) return html
+  return html.replace(/<link href="\/assets\/([^"]+\.css)" rel="stylesheet">/, (a, b) => {
+    return '<style>' + fs.readFileSync('dist/assets/' + b, 'utf8') + '</style>'
+  })
+}
+
 for (const file of Object.keys(files)) {
   const out = path.join('dist', file)
   mkdirp.sync(path.dirname(out))
@@ -13,7 +20,7 @@ for (const file of Object.keys(files)) {
     const html = template.replace(/<!--#(\w+)-->/g, (a, x) => {
       return result[x] || a
     })
-    fs.writeFileSync(out, html)
+    fs.writeFileSync(out, inlineStyle(html))
     console.log('Wriiten to ' + out)
   })
   .catch(e => {
