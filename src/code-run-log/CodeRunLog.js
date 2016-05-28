0a7a03d269fbbdb484b71da0d759a5f85451587f
Thai Pangsakulyanont <dtinth@spacet.me>
2016-05-28 16:46:49 +0700

    Display spec results

--- src/code-run-log/CodeRunLog.js
+++ src/code-run-log/CodeRunLog.js
@@ -6,42 +6,16 @@ import { mapPropsStream } from 'rx-recompose'
 import cx from 'classnames'
 import _ from 'lodash'
 import runCode from '../code-runner/runCode'
+import reducer from './reducer'
 
 function state川ForProps川 (props川) {
-  const initialState = { state: 'running', console: [ ], suiteLevel: 0 }
+  const initialState = reducer(undefined, { type: '@@INIT' })
+
   const textEditors川 = (props川
     .map(props => props.from.map(_.propertyOf(props.state)))
     .distinctUntilChanged(JSON.stringify)
   )
-  function reducer (state, event) {
-    switch (event.type) {
-      case 'reset':
-        return initialState
-      case 'console.log': {
-        const text = event.args.join(' ')
-        return { ...state,
-          console: [ ...state.console,
-            { _id: event._id, type: 'log', text }
-          ]
-        }
-      }
-      case 'error':
-        return { ...state,
-          state: 'error',
-          console: [ ...state.console,
-            { _id: event._id, type: 'error', message: event.message }
-          ]
-        }
-      case 'suite':
-        return { ...state,
-          suiteLevel: state.suiteLevel + 1
-        }
-      case 'completed':
-        return { ...state, state: 'completed' }
-      default:
-        return state
-    }
-  }
+
   return (textEditors川
     .debounce(100)
     .flatMapLatest((code) =>
@@ -66,25 +40,36 @@ const enhance = compose(
   ))
 )
 
+const indent = (level) => ({ marginLeft: (level * 35) })
+
 const CodeRunLog = ({ console, state, error }) => h('pre', {
   styleName: cx('root', {
     isRunning: state === 'running',
     isErrored: state === 'error'
   })
 }, [
-  console.map(entry => {
+  console.map(entry => h('div', { key: entry._id }, [ (() => {
     switch (entry.type) {
       case 'error':
-        return h('div.error', { key: entry._id }, [
+        return h('div.error', { }, [
           h('strong', { }, [ 'Error: ' ]),
           entry.message
         ])
       case 'log':
-        return h('div', { key: entry._id }, entry.text)
+        return h('div', { }, entry.text)
+      case 'suite':
+        return h('.suite', { style: indent(entry.indent) }, entry.title)
+      case 'pass':
+        return h('.pass', { style: indent(entry.indent) }, entry.title)
+      case 'fail':
+        return h('.fail', { style: indent(entry.indent) }, [
+          entry.title,
+          ' (' + entry.message + ')'
+        ])
       default:
         return null
     }
-  })
+  })() ]))
 ])
 
 export default enhance(CodeRunLog)
---
+++ src/code-run-log/reducer.js
@@ -0,0 +1,62 @@
+import u from 'updeep'
+
+const initialState = { state: 'running', console: [ ], suiteLevel: -1 }
+
+export function reducer (state = initialState, event) {
+  return updater(event)(state)
+}
+
+function updater (event) {
+  switch (event.type) {
+    case 'reset':
+      return () => initialState
+    case 'console.log': {
+      const text = event.args.join(' ')
+      return u({
+        console: writeToConsole(event._id, 'log', { text })
+      })
+    }
+    case 'error':
+      return u({
+        state: 'error',
+        console: writeToConsole(event._id, 'error', { message: event.message })
+      })
+    case 'suite':
+      return (state) => u({
+        suiteLevel: (level) => level + 1,
+        console: (state.suiteLevel > -1
+          ? writeToConsole(event._id, 'suite', { title: event.title, indent: state.suiteLevel })
+          : (state) => state
+        )
+      })(state)
+    case 'pass':
+      return (state) => u({
+        console: writeToConsole(event._id, 'pass', {
+          title: event.title,
+          indent: state.suiteLevel - 1
+        })
+      })(state)
+    case 'fail':
+      return (state) => u({
+        console: writeToConsole(event._id, 'fail', {
+          title: event.title,
+          indent: state.suiteLevel - 1,
+          message: event.err.message
+        })
+      })(state)
+    case 'suite end':
+      return u({
+        suiteLevel: (level) => level - 1
+      })
+    case 'completed':
+      return u({ state: 'completed' })
+    default:
+      return (state) => state
+  }
+}
+
+function writeToConsole (_id, type, data) {
+  return (state) => [ ...state, { _id, type, ...data } ]
+}
+
+export default reducer
